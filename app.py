b80e2994a0fbea8481442cc32c10383a25efbd9e
Luc Addis <6288212@edu.vaniercollege.qc.ca>
2025-09-02 12:38:10 -0400

    lab 1

---
+++ app.py
@@ -0,0 +1,11 @@
+name = input("enter your name: ")
+
+age = int(input("enter your age: "))
+
+height = float(input("enter your height: "))
+
+print("Hello, " + name + "!")
+
+print("you are " + str(age) + " years old.")
+
+print("your height is " + str(height) + " meters.")

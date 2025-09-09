5294a4324d9bcdc6d2df7f337b22adc920624921
Luc Addis <6288212@edu.vaniercollege.qc.ca>
2025-09-09 09:51:02 -0400

    Lab-1-added

--- Lab1.py
+++ Lab1.py
@@ -7,30 +7,9 @@ def hello_world():
 # This function should prompt the user for their name (string), age (int), and height (float),
 # and then print them back in a formatted message.
 def input_output():
-    # TODO: Implement this function
-    pass  name = input("enter your name: ")
-
-age = int(input("enter your age: "))
-
-height = float(input("enter your height: "))
-
-print("Hello, " + name + "!")
-
-print("you are " + str(age) + " years old.")
-
-print("your height is " + str(height) + " meters.")
-
-
-# print("Hello, world!)
-
-# name = input("enter your name: ")
-
-# age = int(input("enter your age: "))
-
-# height = float(input("enter your height: "))
-
-# print("Hello, " + name + "!")
-
-# print("you are " + str(age) + " years old.")
-
-# print("your height is " + str(height) + " meters.")
+    name = str(input("Enter your name: "))
+    age = int(input("Enter your age: "))
+    height = float(input("Enter your height: "))
+    print("Hello, " + name + "!")
+    print("You are " + str(age) + " years old.")
+    print("Your height is " + str(height) + " meters.")
